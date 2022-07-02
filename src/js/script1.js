7da4973fda31c6c1cabac9bfaeda6daaea33d47f
Victoriia-Ripka <vikaripka@gmail.com>
2022-07-02 10:59:30 +0200

    9 and 10

--- src/js/script1.js
+++ src/js/script1.js
@@ -300,21 +300,226 @@ const account = {
 
 
 // ========== Модуль 4. Занятие 7. &&& ==========
+function changeEven(numbers, value) {
+  // Change code below this line
+  const newNumbers = [];
+  numbers.forEach( element => { element % 2 === 0 ? newNumbers.push(element + value) : newNumbers.push(element)});
+  return newNumbers;
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   if (numbers[i] % 2 === 0) {
+  //     numbers[i] = numbers[i] + value;
+  //   }
+  // }
+  // Change code above this line
+}
+//console.log(changeEven([1, 2, 3, 4, 5], 10)); //[1, 12, 3, 14, 5
+//console.log(changeEven([2, 8, 3, 7, 4, 6], 10)); //[12, 18, 3, 7, 14, 16]
+
+
 
 // ========== Модуль 4. Занятие 8. &&& ==========
-const bookShelf = {
-  books: ["The last kingdom", "The guardian of dreams"],
-  removeBook(bookName) {
-    const index = this.books.indexOf(bookName);
-    this.books.splice(index, 1);
-    return `Deleting book ${bookName}`;
-  },
-  
-  updateBook(oldName, newName){
-    this.books.splice(oldName, 1, newName);
-    return `Updating book ${oldName} to ${newName}`;
-  },
-};
 
-console.log(bookShelf.removeBook('Red sunset'));
-console.log(bookShelf.updateBook('Sands of dune', 'Dune'));
+
+
+// ========== Модуль 5. Занятие 9. Контекст вызова функции и this ==========
+
+// Example 1 - Мастерская драгоценностей
+// Напишите метод calcTotalPrice(stoneName), который принимает название камня и рассчитывает
+//  и возвращает общую стоимость камней с таким именем, ценой и количеством из свойства stones.
+
+// const chopShop = {
+//   stones: [
+//     { name: 'Emerald', price: 1300, quantity: 4 },
+//     { name: 'Diamond', price: 2700, quantity: 3 },
+//     { name: 'Sapphire', price: 1400, quantity: 7 },
+//     { name: 'Ruby', price: 800, quantity: 2 },
+//   ],
+//   calcTotalPrice(stoneName) {},
+// };
+
+// console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+// console.log(chopShop.calcTotalPrice('Diamond')); // 8100
+// console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
+// console.log(chopShop.calcTotalPrice('Ruby')); // 1600
+
+
+// Example 2 - Телефонная книга
+// Выполните рефакторинг методов объекта phonebook чтобы код заработал.
+
+// const phonebook = {
+//   contacts: [],
+//   add(contact) {
+//     const newContact = {
+//       list: 'default',
+//       ...contact,
+//       id: generateId(),
+//       createdAt: getDate(),
+//     };
+//     contacts.push(newContact);
+//   },
+//   generateId() {
+//     return '_' + Math.random().toString(36).substr(2, 9);
+//   },
+//   getDate() {
+//     return Date.now();
+//   },
+// };
+
+// console.log(
+//   phonebook.add({
+//     name: 'Mango',
+//     email: 'mango@mail.com',
+//     list: 'friends',
+//   }),
+// );
+// console.log(
+//   phonebook.add({
+//     name: 'Poly',
+//     email: 'poly@hotmail.com',
+//   }),
+// );
+
+
+// Example 3 - Калькулятор
+// Создайте объект calculator с тремя методами:
+
+// read(a, b)- принимает два значения и сохраняет их как свойства объекта.
+// add() - возвращает сумму сохранённых значений.
+// mult() - перемножает сохранённые значения и возвращает результат.
+// const calculator = {};
+
+
+
+
+
+
+
+
+
+// ========== Модуль 5. Занятие 10. Прототипы и классы ==========
+
+// Example 1 - Блоггер
+// Напиши класс Blogger для создания обьекта блоггера со следующим свойствами:
+// email - почта, строка
+// age - возраст, число
+// numberOfPosts - кол-во постов, число
+// topics - массив тем на которых специализируется блоггер
+// Класс ожидает один параметр - объект настроек с одноимёнными свойствами.
+// Добавь метод getInfo(), который, возвращает строку: User ${почта} is ${возраст} years 
+// old and has ${ кол - во постов } posts.
+// Добавь метод updatePostCount(value), который в параметре value принимает 
+// количество постов которые нужно добавить пользователю.
+
+// const mango = new User({
+//   name: 'mango@mail.com',
+//   age: 24,
+//   numberOfPosts: 20,
+//   topics: ['tech', 'cooking'],
+// });
+// console.log(mango.getInfo()); // User mango@mail.com is 24 years old and has 20 posts
+// mango.updatePostCount(5);
+// console.log(mango.getInfo()); // User mango@mail.com is 24 years old and has 25 posts
+
+// const poly = new User({
+//   name: 'poly@mail.com',
+//   age: 19,
+//   numberOfPosts: 17,
+//   topics: ['sports', 'gaming', 'health'],
+// });
+// console.log(poly.getInfo()); // User poly@mail.com is 19 years old and has 17 posts
+// poly.updatePostCount(4);
+// console.log(poly.getInfo()); // User poly@mail.com is 19 years old and has 21 posts
+
+
+// Example 2 - Хранилище
+// Напиши класс Storage который создаёт объекты для управления складом товаров. 
+// При вызове будет получать один аргумент - начальный массив товаров,
+// и записывать его в свойство items.
+// Добавь методы класса:
+
+// getItems() - возвращает массив товаров.
+// addItem(item) - получает новый товар и добавляет его к текущим.
+// removeItem(item) - получает товар и, если он есть, удаляет его из текущих.
+// const storage = new Storage(['🍎', '🍋', '🍇', '🍑']);
+
+// const items = storage.getItems();
+// console.table(items); // [ '🍎', '🍋', '🍇', '🍑' ]
+
+// storage.addItem('🍌');
+// console.table(storage.items); // [ '🍎', '🍋', '🍇', '🍑', '🍌' ]
+
+// storage.removeItem('🍋');
+// console.table(storage.items); // [ '🍎', '🍇', '🍑', '🍌' ]
+
+// Example 3 - User
+// Напиши класс User который создаёт объект со свойствами login и email. 
+// Объяви приватные свойства #login и #email, доступ к которым сделай через геттер 
+// и сеттер login и email.
+
+// const mango = new User({
+//   login: 'Mango',
+//   email: 'mango@dog.woof',
+// });
+
+// console.log(mango.login); // Mango
+// mango.login = 'Mangodoge';
+// console.log(mango.login); // Mangodoge
+
+// const poly = new User({
+//   login: 'Poly',
+//   email: 'poly@mail.com',
+// });
+
+// console.log(poly.login); // Poly
+// poly.login = 'Polycutie';
+// console.log(poly.login); // Polycutie
+
+
+// Example 4 - Заметки
+// Напиши класс Notes который управляет коллекцией заметок в свойстве items. 
+//Заметка это объект со свойствами text и priority.Добавь классу статическое свойство Priority,
+// в котором будет храниться объект с приоритетами.
+
+// {
+//   LOW: 'low',
+//   NORMAL: 'normal',
+//   HIGH: 'high'
+// }
+// Добавь методы addNote(note), removeNote(text) и updatePriority(text, newPriority).
+
+// const myNotes = new Notes([]);
+
+// myNotes.addNote({ text: 'Моя первая заметка', priority: Notes.Priority.LOW });
+// console.log(myNotes.items);
+
+// myNotes.addNote({
+//   text: 'Моя вторая заметка',
+//   priority: Notes.Priority.NORMAL,
+// });
+// console.log(myNotes.items);
+
+// myNotes.removeNote('Моя первая заметка');
+// console.log(myNotes.items);
+
+// myNotes.updateNote('Моя вторая заметка', Notes.Priority.HIGH);
+// console.log(myNotes.items);
+
+
+// Example 5 - Toggle
+// Напишите класс Toggle который принимает объект настроек { isOpen: boolean } 
+// и объявляет одно свойство on - состояние вкл / выкл(true / false).
+// По умолчанию значение свойства on должно быть false.
+
+// const firstToggle = new Toggle({ isOpen: true });
+// console.group('firstToggle');
+// console.log(firstToggle.on);
+// firstToggle.toggle();
+// console.log(firstToggle.on);
+// console.groupEnd('firstToggle');
+
+// const secondToggle = new Toggle();
+// console.group('secondToggle');
+// console.log(secondToggle.on);
+// secondToggle.toggle();
+// console.log(secondToggle.on);
+// console.groupEnd('secondToggle');
